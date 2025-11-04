668fd147dd192cbfd9da36b6be455cf00c228ae8
Thomas Tverskov Kjær <thomas@zandora.net>
2025-11-04 15:53:16 +0100

    About finalized. Cz language support added

---
+++ dev/locale/cs/language_cs.php
@@ -0,0 +1,462 @@
+<?php
+
+$current_anatomy = [
+    "anatomy-female-vulva" => [
+        "label" => "Vulva",
+        "description" => null
+    ],
+    "anatomy-male-penis" => [
+        "label" => "Penis",
+        "description" => null
+    ],
+    "anatomy-mtf-1" => [
+        "label" => "Transgenderová vulva po operaci",
+        "description" => "Chirurgicky vytvořená vulva po operaci potvrzení pohlaví."
+    ],
+    "anatomy-mtf-2" => [
+        "label" => "Hormonální terapie / bez operace",
+        "description" => "Osoby, které podstupují hormonální terapii, ale nemají za sebou operaci."
+    ],
+    "anatomy-mtf-3" => [
+        "label" => "Orchiektomie (odstranění varlat)",
+        "description" => "Chirurgické odstranění varlat bez kompletní vaginoplastiky."
+    ],
+    "anatomy-mtf-4" => [
+        "label" => "Hrudník s prsy",
+        "description" => "Vyvinutá prsa v důsledku hormonální terapie nebo zvětšení prsou."
+    ],
+    "anatomy-ftm-1" => [
+        "label" => "Transgenderový penis po operaci",
+        "description" => "Chirurgicky vytvořený penis po operaci potvrzení pohlaví."
+    ],
+    "anatomy-ftm-2" => [
+        "label" => "Hormonální terapie / bez operace",
+        "description" => "Osoby, které podstupují hormonální terapii, ale nemají za sebou operaci."
+    ],
+    "anatomy-ftm-3" => [
+        "label" => "Ploché poprsí (po operaci)",
+        "description" => "Chirurgicky zploštělý hrudník po operaci hrudníku (top surgery)."
+    ],
+];
+
+$additional_information = [
+    "chest-hair-natural" => [
+        "label" => "Přirozené",
+        "description" => "Chlupy jsou ponechány zcela přirozeně a rostou volně."
+    ],
+    "chest-hair-trimmed" => [
+        "label" => "Zastřižené",
+        "description" => "Chlupy jsou záměrně zkrácené nebo upravené (např. nůžkami nebo strojkem), ale nejsou úplně odstraněné."
+    ],
+    "chest-hair-hairless" => [
+        "label" => "Bez chlupů",
+        "description" => "Oblast je úplně nebo téměř bez chlupů, včetně holení, voskování, laseru nebo podobného postupu. Lehký strniště se stále počítá jako bez chlupů."
+    ],
+
+    "genital-hair-above-natural" => [
+        "label" => "Přirozené",
+        "description" => "Chlupy jsou ponechány zcela přirozeně a rostou volně."
+    ],
+    "genital-hair-above-trimmed" => [
+        "label" => "Zastřižené",
+        "description" => "Chlupy jsou záměrně zkrácené nebo upravené (např. nůžkami nebo strojkem), ale nejsou úplně odstraněné."
+    ],
+    "genital-hair-above-hairless" => [
+        "label" => "Bez chlupů",
+        "description" => "Oblast je úplně nebo téměř bez chlupů, včetně holení, voskování, laseru nebo podobného postupu. Lehký strniště se stále počítá jako bez chlupů."
+    ],
+
+    "genital-hair-natural" => [
+        "label" => "Přirozené",
+        "description" => "Chlupy jsou ponechány zcela přirozeně a rostou volně."
+    ],
+    "genital-hair-trimmed" => [
+        "label" => "Zastřižené",
+        "description" => "Chlupy jsou záměrně zkrácené nebo upravené (např. nůžkami nebo strojkem), ale nejsou úplně odstraněné."
+    ],
+    "genital-hair-hairless" => [
+        "label" => "Bez chlupů",
+        "description" => "Oblast je úplně nebo téměř bez chlupů, včetně holení, voskování, laseru nebo podobného postupu. Lehký strniště se stále počítá jako bez chlupů."
+    ],
+
+    "buttocks-hair-natural" => [
+        "label" => "Přirozené",
+        "description" => "Chlupy jsou ponechány zcela přirozeně a rostou volně."
+    ],
+    "buttocks-hair-trimmed" => [
+        "label" => "Zastřižené",
+        "description" => "Chlupy jsou záměrně zkrácené nebo upravené (např. nůžkami nebo strojkem), ale nejsou úplně odstraněné."
+    ],
+    "buttocks-hair-hairless" => [
+        "label" => "Bez chlupů",
+        "description" => "Oblast je úplně nebo téměř bez chlupů, včetně holení, voskování, laseru nebo podobného postupu. Lehký strniště se stále počítá jako bez chlupů."
+    ],
+
+    "marks-none" => [
+        "label" => "Žádné strie ani jizvy",
+        "description" => null
+    ],
+    "marks-stretch" => [
+        "label" => "Strie",
+        "description" => null
+    ],
+    "marks-scars" => [
+        "label" => "Jizvy",
+        "description" => null
+    ],
+
+    "pregnancy-true" => [
+        "label" => "Ano",
+        "description" => null
+    ],
+    "pregnancy-false" => [
+        "label" => "Ne",
+        "description" => null
+    ],
+
+    "vaginal-birth-true" => [
+        "label" => "Ano",
+        "description" => null
+    ],
+    "vaginal-birth-false" => [
+        "label" => "Ne",
+        "description" => null
+    ],
+
+    "c-section-true" => [
+        "label" => "Ano",
+        "description" => null
+    ],
+    "c-section-false" => [
+        "label" => "Ne",
+        "description" => null
+    ],
+
+    "breastfeeding-current" => [
+        "label" => "Aktuálně",
+        "description" => null
+    ],
+    "breastfeeding-past" => [
+        "label" => "V minulosti",
+        "description" => null
+    ],
+    "breastfeeding-false" => [
+        "label" => "Ne",
+        "description" => null
+    ],
+
+    "piercings-false" => [
+        "label" => "Žádné piercingy",
+        "description" => null
+    ],
+    "piercings-nipple" => [
+        "label" => "Bradavka",
+        "description" => null
+    ],
+    "piercings-genital" => [
+        "label" => "Genitálie",
+        "description" => null
+    ],
+    "piercings-buttocks" => [
+        "label" => "Hýždě",
+        "description" => null
+    ],
+    "piercings-other" => [
+        "label" => "Jiné",
+        "description" => null
+    ],
+
+    "tattoos-false" => [
+        "label" => "Žádná tetování",
+        "description" => null
+    ],
+    "tattoos-breasts" => [
+        "label" => "Tetování na prsou",
+        "description" => null
+    ],
+    "tattoos-vulva" => [
+        "label" => "Tetování na vulvě",
+        "description" => null
+    ],
+    "tattoos-penis" => [
+        "label" => "Tetování na penisu",
+        "description" => null
+    ],
+    "tattoos-buttocks" => [
+        "label" => "Tetování na hýždích",
+        "description" => null
+    ],
+    "tattoos-other" => [
+        "label" => "Jiná tetování",
+        "description" => null
+    ],
+
+    "hormonal-influence-true" => [
+        "label" => "Ano",
+        "description" => null
+    ],
+    "hormonal-influence-false" => [
+        "label" => "Ne",
+        "description" => null
+    ],
+
+    "menstrual-cycle-true" => [
+        "label" => "Ano",
+        "description" => null
+    ],
+    "menstrual-cycle-false" => [
+        "label" => "Ne",
+        "description" => null
+    ],
+];
+
+// All countries in Czech
+$country_list = array(
+    NULL => '',
+    'AF' => 'Afghánistán',
+    'AX' => 'Ålandy',
+    'AL' => 'Albánie',
+    'DZ' => 'Alžírsko',
+    'AS' => 'Americká Samoa',
+    'AD' => 'Andorra',
+    'AO' => 'Angola',
+    'AI' => 'Anguilla',
+    'AQ' => 'Antarktida',
+    'AG' => 'Antigua a Barbuda',
+    'AR' => 'Argentina',
+    'AM' => 'Arménie',
+    'AW' => 'Aruba',
+    'AU' => 'Austrálie',
+    'AT' => 'Rakousko',
+    'AZ' => 'Ázerbájdžán',
+    'BS' => 'Bahamy',
+    'BH' => 'Bahrajn',
+    'BD' => 'Bangladéš',
+    'BB' => 'Barbados',
+    'BY' => 'Bělorusko',
+    'BE' => 'Belgie',
+    'BZ' => 'Belize',
+    'BJ' => 'Benin',
+    'BM' => 'Bermudy',
+    'BT' => 'Bhútán',
+    'BO' => 'Bolívie',
+    'BA' => 'Bosna a Hercegovina',
+    'BW' => 'Botswana',
+    'BV' => 'Bouvetův ostrov',
+    'BR' => 'Brazílie',
+    'IO' => 'Britské indickooceánské území',
+    'VG' => 'Britské Panenské ostrovy',
+    'BN' => 'Brunej',
+    'BG' => 'Bulharsko',
+    'BF' => 'Burkina Faso',
+    'BI' => 'Burundi',
+    'KH' => 'Kambodža',
+    'CM' => 'Kamerun',
+    'CA' => 'Kanada',
+    'CV' => 'Kapverdy',
+    'KY' => 'Kajmanské ostrovy',
+    'CF' => 'Středoafrická republika',
+    'TD' => 'Čad',
+    'CL' => 'Chile',
+    'CN' => 'Čína',
+    'CX' => 'Vánoční ostrov',
+    'CC' => 'Kokosové (Keelingovy) ostrovy',
+    'CO' => 'Kolumbie',
+    'KM' => 'Komory',
+    'CD' => 'Kongo, demokratická republika',
+    'CG' => 'Kongo, republika',
+    'CK' => 'Cookovy ostrovy',
+    'CR' => 'Kostarika',
+    'CI' => 'Pobřeží slonoviny',
+    'HR' => 'Chorvatsko',
+    'CU' => 'Kuba',
+    'CY' => 'Kypr',
+    'CZ' => 'Česká republika',
+    'DK' => 'Dánsko',
+    'DJ' => 'Džibutsko',
+    'DM' => 'Dominika',
+    'DO' => 'Dominikánská republika',
+    'EC' => 'Ekvádor',
+    'EG' => 'Egypt',
+    'SV' => 'Salvador',
+    'GQ' => 'Rovníková Guinea',
+    'ER' => 'Eritrea',
+    'EE' => 'Estonsko',
+    'ET' => 'Etiopie',
+    'FO' => 'Faerské ostrovy',
+    'FK' => 'Falklandy (Malvíny)',
+    'FJ' => 'Fidži',
+    'FI' => 'Finsko',
+    'FR' => 'Francie',
+    'GF' => 'Francouzská Guyana',
+    'PF' => 'Francouzská Polynésie',
+    'TF' => 'Francouzská jižní a antarktická území',
+    'GA' => 'Gabon',
+    'GM' => 'Gambie',
+    'GE' => 'Gruzie',
+    'DE' => 'Německo',
+    'GH' => 'Ghana',
+    'GI' => 'Gibraltar',
+    'GR' => 'Řecko',
+    'GL' => 'Grónsko',
+    'GD' => 'Grenada',
+    'GP' => 'Guadeloupe',
+    'GU' => 'Guam',
+    'GT' => 'Guatemala',
+    'GG' => 'Guernsey',
+    'GN' => 'Guinea',
+    'GW' => 'Guinea-Bissau',
+    'GY' => 'Guyana',
+    'HT' => 'Haiti',
+    'HM' => 'Heardův ostrov a McDonaldovy ostrovy',
+    'VA' => 'Vatikán',
+    'HN' => 'Honduras',
+    'HK' => 'Hongkong',
+    'HU' => 'Maďarsko',
+    'IS' => 'Island',
+    'IN' => 'Indie',
+    'ID' => 'Indonésie',
+    'IR' => 'Írán',
+    'IQ' => 'Irák',
+    'IE' => 'Irsko',
+    'IM' => 'Ostrov Man',
+    'IL' => 'Izrael',
+    'IT' => 'Itálie',
+    'JM' => 'Jamajka',
+    'JP' => 'Japonsko',
+    'JE' => 'Jersey',
+    'JO' => 'Jordánsko',
+    'KZ' => 'Kazachstán',
+    'KE' => 'Keňa',
+    'KI' => 'Kiribati',
+    'KP' => 'Severní Korea',
+    'KR' => 'Jižní Korea',
+    'KW' => 'Kuvajt',
+    'KG' => 'Kyrgyzstán',
+    'LA' => 'Laos',
+    'LV' => 'Lotyšsko',
+    'LB' => 'Libanon',
+    'LS' => 'Lesotho',
+    'LR' => 'Libérie',
+    'LY' => 'Libye',
+    'LI' => 'Lichtenštejnsko',
+    'LT' => 'Litva',
+    'LU' => 'Lucembursko',
+    'MO' => 'Macao',
+    'MK' => 'Severní Makedonie',
+    'MG' => 'Madagaskar',
+    'MW' => 'Malawi',
+    'MY' => 'Malajsie',
+    'MV' => 'Maledivy',
+    'ML' => 'Mali',
+    'MT' => 'Malta',
+    'MH' => 'Marshallovy ostrovy',
+    'MQ' => 'Martinik',
+    'MR' => 'Mauritánie',
+    'MU' => 'Mauricius',
+    'YT' => 'Mayotte',
+    'MX' => 'Mexiko',
+    'FM' => 'Mikronésie',
+    'MD' => 'Moldavsko',
+    'MC' => 'Monako',
+    'MN' => 'Mongolsko',
+    'ME' => 'Černá Hora',
+    'MS' => 'Montserrat',
+    'MA' => 'Maroko',
+    'MZ' => 'Mosambik',
+    'MM' => 'Myanmar (Barma)',
+    'NA' => 'Namibie',
+    'NR' => 'Nauru',
+    'NP' => 'Nepál',
+    'AN' => 'Nizozemské Antily',
+    'NL' => 'Nizozemsko',
+    'NC' => 'Nová Kaledonie',
+    'NZ' => 'Nový Zéland',
+    'NI' => 'Nikaragua',
+    'NE' => 'Niger',
+    'NG' => 'Nigérie',
+    'NU' => 'Niue',
+    'NF' => 'Norfolk',
+    'MP' => 'Severní Mariany',
+    'NO' => 'Norsko',
+    'OM' => 'Omán',
+    'PK' => 'Pákistán',
+    'PW' => 'Palau',
+    'PS' => 'Palestina',
+    'PA' => 'Panama',
+    'PG' => 'Papua Nová Guinea',
+    'PY' => 'Paraguay',
+    'PE' => 'Peru',
+    'PH' => 'Filipíny',
+    'PN' => 'Pitcairnovy ostrovy',
+    'PL' => 'Polsko',
+    'PT' => 'Portugalsko',
+    'PR' => 'Portoriko',
+    'QA' => 'Katar',
+    'RE' => 'Réunion',
+    'RO' => 'Rumunsko',
+    'RU' => 'Rusko',
+    'RW' => 'Rwanda',
+    'BL' => 'Svatý Bartoloměj',
+    'SH' => 'Svatá Helena',
+    'KN' => 'Svatý Kryštof a Nevis',
+    'LC' => 'Svatá Lucie',
+    'MF' => 'Svatý Martin',
+    'PM' => 'Saint Pierre a Miquelon',
+    'VC' => 'Svatý Vincenc a Grenadiny',
+    'WS' => 'Samoa',
+    'SM' => 'San Marino',
+    'ST' => 'Svatý Tomáš a Princův ostrov',
+    'SA' => 'Saúdská Arábie',
+    'SN' => 'Senegal',
+    'RS' => 'Srbsko',
+    'SC' => 'Seychely',
+    'SL' => 'Sierra Leone',
+    'SG' => 'Singapur',
+    'SK' => 'Slovensko',
+    'SI' => 'Slovinsko',
+    'SB' => 'Šalamounovy ostrovy',
+    'SO' => 'Somálsko',
+    'ZA' => 'Jihoafrická republika',
+    'GS' => 'Jižní Georgie a Jižní Sandwichovy ostrovy',
+    'ES' => 'Španělsko',
+    'LK' => 'Srí Lanka',
+    'SD' => 'Súdán',
+    'SR' => 'Surinam',
+    'SJ' => 'Špicberky a Jan Mayen',
+    'SZ' => 'Eswatini (Svazijsko)',
+    'SE' => 'Švédsko',
+    'CH' => 'Švýcarsko',
+    'SY' => 'Sýrie',
+    'TW' => 'Tchaj-wan',
+    'TJ' => 'Tádžikistán',
+    'TZ' => 'Tanzanie',
+    'TH' => 'Thajsko',
+    'TL' => 'Východní Timor',
+    'TG' => 'Togo',
+    'TK' => 'Tokelau',
+    'TO' => 'Tonga',
+    'TT' => 'Trinidad a Tobago',
+    'TN' => 'Tunisko',
+    'TR' => 'Turecko',
+    'TM' => 'Turkmenistán',
+    'TC' => 'Ostrovy Turks a Caicos',
+    'TV' => 'Tuvalu',
+    'UG' => 'Uganda',
+    'UA' => 'Ukrajina',
+    'AE' => 'Spojené arabské emiráty',
+    'GB' => 'Spojené království',
+    'US' => 'Spojené státy',
+    'UM' => 'Menší odlehlé ostrovy USA',
+    'VI' => 'Americké Panenské ostrovy',
+    'UY' => 'Uruguay',
+    'UZ' => 'Uzbekistán',
+    'VU' => 'Vanuatu',
+    'VE' => 'Venezuela',
+    'VN' => 'Vietnam',
+    'WF' => 'Wallis a Futuna',
+    'EH' => 'Západní Sahara',
+    'YE' => 'Jemen',
+    'ZM' => 'Zambie',
+    'ZW' => 'Zimbabwe'
+);
